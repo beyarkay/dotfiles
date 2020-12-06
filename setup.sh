7ce17889f8e6b2484092e6c5858d899517bb81cf
beyarkay <boydrkane@gmail.com>
2020-12-06 13:00:47 +0200

    fix: Make the script function properly

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
-FILES=".vimrc .bash_profile .gitconfig"
-for f in $FILES 
+TO_COPY=".bash_profile .gitconfig .gitmessage .vimrc .zshrc "
+TO_SOURCE=".bash_profile .zshrc"
+for f in $TO_COPY 
 do 
-    echo "Copying $f file"
-    cp $f ~/
+    echo "$f"
+    if [[ -f "~/$f" ]]; then
+       echo "Moving existing filoe ~/$f to ~/old_$f"
+       mv ~/$f "~/old_$f"
+    fi
+    cp $f ~/$f
+done
+
+# Now Source the relevant files
+for f in $TO_SOURCE 
+do 
+    echo "$f"
+    if [[ -f "~/$f" ]]; then
+       echo "Sourcing ~/$f"
+       source ~/$f
+    fi
 done
