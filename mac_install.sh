8363beda448b25280abb0abf0223abf8c55fb801
Boyd Kane <boydrkane@gmail.com>
2021-11-20 15:06:13 +0000

    Add TODOs for mac_install

--- mac_install.sh
+++ mac_install.sh
@@ -1,1 +1,6 @@
 echo -e "$RED No Mac install scripts are setup yet"
+# TODO: build vim from source
+# TODO: install homebrew
+# TODO: install gh client
+# TODO: install tmux
+# TODO: remap caps-lock to ctrl
