9c5cc0044eaf916f2aac091730eedcd43e48e16c
beyarkay <boydrkane@gmail.com>
2022-01-14 16:09:03 +0200

    Add gree outline

---
+++ unstable/gree.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# gree - Git-aware tree
+#
+# This command should output a `tree` like structure, but also show the git
+# status of the various files. Preferably it should also use colours in its
+# output
+echo "Not yet implemented"
