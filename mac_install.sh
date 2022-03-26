a284e999166b15f5c66fcb61703ef2f29196f8e5
beyarkay <boydrkane@gmail.com>
2022-03-26 14:17:45 +0200

    Add more space for inline comments

--- mac_install.sh
+++ mac_install.sh
@@ -269,17 +269,17 @@ if [ -x "$(command -v brew)" ]; then
     # Install those packages now.
     # ==================================================================
     caskable=(
-        alfred          # Productivity manager
-        arduino         # Arduino.cc platform
-        firefox         # Web browser
-        font-iosevka    # Monospace font with really nice ligatures
-        freecad         # FOSS CAD software for 3D printing
-        google-chrome   # Backup web browser for web scraping
-        iterm2          # Better Terminal.app replacement
-        rectangle       # MacOS window tiling manager
-        spotify         # Music player
-        vlc             # Video player
-        whatsapp        # Instant messaging
+        alfred                 # Productivity manager
+        arduino                # Arduino.cc platform
+        firefox                # Web browser
+        font-iosevka           # Monospace font with really nice ligatures
+        freecad                # FOSS CAD software for 3D printing
+        google-chrome          # Backup web browser for web scraping
+        iterm2                 # Better Terminal.app replacement
+        rectangle              # MacOS window tiling manager
+        spotify                # Music player
+        vlc                    # Video player
+        whatsapp               # Instant messaging
     )
     for c in "${caskable[@]}"
     do
